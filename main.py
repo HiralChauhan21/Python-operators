d719a602928203c83aa81877caca6f9c3a01e471
HiralChauhan21 <vishal.chauhan.home@gmail.com>
2024-11-10 13:15:38 +0530

    commit

---
+++ main.py
@@ -0,0 +1,22 @@
+num1= 45
+num2 = 3
+
+print("num1: ", num1)
+print("num2: ", num2)
+
+print("sum: ", num1+num2)
+print("difference", num1-num2)
+print("product: ", num1*num2)
+print("division: ", num1/num2)
+print("floor division: ", num1//num2)
+print("Modulus division: ", num1%num2)
+print("square: ", num2**2)
+print("square root: ", num1**0.5)
+
+print("equal? ", num1==num2)
+print("num1 greater ", num1>num2)
+print("num2 greater ", num2>num1)
+print("not equal? ", num1!=num2)
+
+result = num1/2+num2**2+10
+print("result of the equation is ", result)
